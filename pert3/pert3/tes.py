fb095a4f10f9f0183fce9fe1f247ed191048c7b9
putradaffad <ptradaffa45@student.esaunggul.ac.id>
2024-05-07 10:05:28 +0700

    pert3

--- pert3/pert3/tes.py
+++ pert3/pert3/tes.py
@@ -1,15 +1,14 @@
-value_str = input("enter value : ")
-value = int(value_str)
-
-
+# value_str = input("enter value : ")
+# value = int(value_str)
+value = int(input("enter value : "))
 if value >= 90:
     grade = "A"
 elif value >= 80:
     grade = "B"
 elif value >= 70:
-    grade = "B"
+    grade = "C"
 elif value >= 60:
-    grade = "B"
+    grade = "D"
 else :
     grade = "E"
 
