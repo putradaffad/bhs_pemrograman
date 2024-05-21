d9d75059e9fb9ee2fbf6e2f127d09525fc7d1e65
putradaffad <ptradaffa45@student.esaunggul.ac.id>
2024-05-21 14:43:26 +0700

    UTS

---
+++ UTS/soal4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int umur = 21;
+    cout << "Berarti Usia Kalian sekarang adalah " << umur << " Tahun";
+
+    return 0;
+}
+
+//Output : Berarti Usia Kalian sekarang adalah 21 Tahun
